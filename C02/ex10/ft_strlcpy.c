e4d4d29b3cfc68ae3d9ddf1eb0c95f4c51eb47fc
Abd-7-Oul <139695697+Abd-7-Oul@users.noreply.github.com>
2025-07-31 10:22:26 +0100

    Add files via upload

---
+++ C02/ex10/ft_strlcpy.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aboulfid <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/21 13:25:47 by aboulfid          #+#    #+#             */
+/*   Updated: 2025/07/22 09:59:50 by aboulfid         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned	int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	unsigned int	i;
+	unsigned int	j;
+
+	i = 0;
+	j = 0;
+	if (size > 0)
+	{
+		while (src[i] != '\0' && i < (size - 1))
+		{
+			dest[i] = src[i];
+			i++;
+		}
+		dest[i] = '\0';
+	}
+	while (src[j] != '\0')
+	{
+		j++;
+	}
+	return (j);
+}
